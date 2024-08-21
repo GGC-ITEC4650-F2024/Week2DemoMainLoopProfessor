083c3f96e6f4e1115d0ef78d57f759a407501217
Sebastien Siva <ssiva@ggc.edu>
2024-08-21 13:18:31 +0000

    Lecture on 8/20

--- DemoCSharpProj/ConsoleApp1/Program.cs
+++ DemoCSharpProj/ConsoleApp1/Program.cs
@@ -4,8 +4,46 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Fruit f = new Fruit("Pear", 1.05);
-        Console.WriteLine(f);
+        Console.WriteLine("Keep taking cards up to 21. If you go over, you lose.");
+
+        Random rgen = new Random();
+
+        //game state
+        int compScore = rgen.Next(15, 20);
+        int playerScore = 0;
+        string playerCards = "";
+        bool gameOver = false;
+        
+        //deal 1 card
+        int card = rgen.Next(1, 11);
+        playerScore += card;
+        playerCards += card + " ";
+        
+        //main loop
+        while(!gameOver) {
+            //render
+            Console.WriteLine("Current Cards: " + playerCards);
+            Console.WriteLine("Current Score: " + playerScore);
+            Console.Write("Want another card (y or n)? ");
+
+            //get user input
+            string cmd = Console.ReadLine().ToUpper();
+
+            //update state
+            if(cmd == "Y") {
+                card = rgen.Next(1, 11);
+                playerScore += card;
+                playerCards += card + " ";
+            }
+            if(playerScore > 21 || cmd != "Y" ) {
+                gameOver = true;
+            }
+        }
+
+        if (playerScore > compScore && playerScore <= 21) {
+            Console.WriteLine("Winner Winner Chicken Dinner!");
+        } else {
+            Console.WriteLine("Wasted!");
+        }
     }
 }
